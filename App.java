0ee7f79de9d4fba431e23ad4d0e788b5c0ffb9df
snvardaro <snvardaro26@gmail.com>
2016-04-11 13:13:59 -0400

    Update App.java

--- App.java
+++ App.java
@@ -5,8 +5,17 @@ import Inventory.java;
 
 public class App 
 {
+	public void ItemLog();
+	
 	public static void main(String[] args)
 	{
 		ItemLog();
 	}
+	
+	/*
+	void ItemLog()
+	{
+		//Sets all items into an array	
+	}
+	*/
 }
