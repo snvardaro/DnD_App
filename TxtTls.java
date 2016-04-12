fa95c16771df4fcc4ea9e67fd198cd32b88bdd33
snvardaro <snvardaro26@gmail.com>
2016-04-12 09:32:53 -0400

    Create TxtTls.java

---
+++ TxtTls.java
@@ -0,0 +1,13 @@
+import java.io.Scanner
+
+public class TxtTls
+{
+	public static < E > void out(E thing)
+	{
+		System.out.println(string);
+	}
+	public static < E > void testmsg(E thing)
+	{
+		System.out.println("TEST MSG: " + string);
+	}
+}
